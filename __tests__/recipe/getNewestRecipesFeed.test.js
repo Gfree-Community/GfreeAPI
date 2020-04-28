06d2cc5b66ee9265c5b2309f762dba351020b1ee
Aminejvm <elouartinra@hotmail.com>
2020-04-28 18:43:43 +0000

    Added test to assure getNewestRecipesFeed  sends the correct properties

--- __tests__/recipe/getNewestRecipesFeed.test.js
+++ __tests__/recipe/getNewestRecipesFeed.test.js
@@ -1,13 +1,34 @@
 const request = require("supertest");
 const app = require("../../server");
 
+const getNewestRecipesFeed = request(app).get("/getNewestRecipesFeed");
+
 describe("getNewestRecipesFeed Endpoint", () => {
   it("Should Show 5 recipes", async () => {
-    const res = await request(app).get("/getNewestRecipesFeed").query({
+    const res = await getNewestRecipesFeed.query({
       count: 5,
       page: 1,
     });
     expect(res.statusCode).toEqual(200);
     expect(res.body.recipe.length).toEqual(5);
+  });
+
+  it("Should have Recipes with Required Properties", async () => {
+    const res = await getNewestRecipesFeed;
+    res.body.recipe.forEach((item) => {
+      expect(item).toEqual(
+        expect.objectContaining({
+          _id: expect.any(String),
+          title: expect.any(String),
+          body: expect.any(Array),
+          author: expect.any(String),
+          thumbnail: expect.any(String),
+          createdAt: expect.any(String),
+          cookingTime: expect.any(String),
+          description: expect.any(String),
+          tags: expect.any(Array),
+        })
+      );
+    });
   });
 });
