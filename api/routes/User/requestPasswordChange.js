2ca71ff43f71c4356504245f1e4e68d8e7622cda
benhakimilyass <ilyassbenhakim2@gmail.com>
2020-05-14 00:29:11 +0000

    Nodemailer not working

--- api/routes/User/requestPasswordChange.js
+++ api/routes/User/requestPasswordChange.js
@@ -1,13 +1,22 @@
 const router = require("express").Router();
-const sgMail = require("@sendgrid/mail");
 const jwt = require("jsonwebtoken");
 const User = require("../../controllers/User");
+const nodemailer = require("nodemailer");
 
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-const RequestMsg = ({ token, email }) => ({
+/**Gfree email configs */
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "ilyassbenhakim2@gmail.com",
+    pass: "zgycuieidwwlzaev",
+  },
+});
+
+/**Mail payload */
+const mailOptions = ({ token, email }) => ({
+  from: "ilyassbenhakim2@gmail.com",
   to: email,
-  from: "test@example.com",
-  subject: "Gfree,Password Reset Request",
+  subject: "Gfree, Password Reset Request",
   text: "Click the link below to reset your password",
   html: `<strong>
   <a href={http:localhost:3000/profile/change-password?token=${token}}>Change Password</a></strong>`,
@@ -25,12 +34,8 @@ const requestPasswordChange = router.body("/", async (req, res, next) => {
   const token = jwt.sign({ id: email }, "t7m08", {
     expiresIn: "24h",
   });
-  await sgMail.send(
-    RequestMsg({
-      email,
-      token,
-    })
-  );
+
+  await transporter.sendMail(mailOptions({ token, email }));
   res.status(201);
 });
 
