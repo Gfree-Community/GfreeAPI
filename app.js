e431909c23820378c71d42a43d2eecba06374db8
omar fertat <iroonix5@gmail.com>
2021-07-22 01:54:57 +0100

    Update app.js
    changed mongodb atlas database link

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ process.env.NODE_ENV !== "test" &&
   mongoose.connect(
     "mongodb+srv://jlo:" +
       pwddb +
-      "@gfreedb.jfg6j.mongodb.net/<dbname>?retryWrites=true&w=majority",
+      "@gfree.5rmfi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
